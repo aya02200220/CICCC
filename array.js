d29755222fbc773d349e4f4caf18df2c1e6b9d22
Aya <pyon220@gmail.com>
2022-09-21 14:44:38 -0700

    0921

--- array.js
+++ array.js
@@ -20,3 +20,12 @@ for (let i = 0; i < objArr.length; i++) {
   console.log(objArr[i].name);
   console.log(objArr[i].lastName);
 }
+
+//Duplicate array
+let arr2 = [0, 1, 2, 3, 4];
+let arrTemp = Array.from(arr2);
+
+arr2[0] = 50;
+arrTemp[1] = 100;
+console.log(arr2); //[50,1,2,3,4]
+console.log(arrTemp); //[0,100,2,3,4]
